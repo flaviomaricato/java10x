ceca717cccfec521c27bf28bc044f2d3756e8dcf
flaviomaricato@gmail.com <flaviomaricato@gmail.com>
2025-01-14 14:21:56 -0300

    Principais shortcuts + Configuração do InteliJ

--- JavaNinja/src/TiposDeDados/DadosPrimitivos.java
+++ JavaNinja/src/TiposDeDados/DadosPrimitivos.java
@@ -8,11 +8,15 @@ public class DadosPrimitivos {
         * Objetivo da aula: Criar um ninja - Naruto -
         **/
 
-        int view  = 2147483648; // Valor máximo: 2 147 483 647
+        int idade  = 17; // Valor máximo: 2 147 483 647
         double altura = 1.65;
         char inicial = 'N';
         boolean vivoOuMorto = true;
         Long saldoBancario = 20147483649L; // Valor máximo: 9,223,372,036,854,775,807
 
+        // sout e soutv
+        System.out.println(idade);
+        System.out.println(saldoBancario);
+        System.out.println("saldoBancario = " + saldoBancario);
     }
 }
