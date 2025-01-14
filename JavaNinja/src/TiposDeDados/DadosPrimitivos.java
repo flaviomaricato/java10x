328ce3f557a83753e1a4cdf3b7827cfcde1a5dd4
Flavio <Maricato>
2025-01-14 06:53:27 -0300

    first commit

---
+++ JavaNinja/src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,18 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+
+        /*
+        * Dados primitivos - int, double, float, char, boolean, short.
+        * Objetivo da aula: Criar um ninja - Naruto -
+        **/
+
+        int view  = 2147483647; // Valor máximo: 2 147 483 647
+        double altura = 1.65;
+        char inicial = 'N';
+        boolean vivoOuMorto = true;
+        Long saldoBancario = 20147483649L; // Valor máximo: 9,223,372,036,854,775,807
+
+    }
+}
