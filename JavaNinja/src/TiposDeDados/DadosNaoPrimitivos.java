86e0e7c6f17f9d22929393a5cbbd4be98ca26cfe
Flavio <Maricato>
2025-01-19 10:13:17 -0300

    Dados não primitivos

--- JavaNinja/src/TiposDeDados/DadosNaoPrimitivos.java
+++ JavaNinja/src/TiposDeDados/DadosNaoPrimitivos.java
@@ -2,6 +2,16 @@ package TiposDeDados;
 
 public class DadosNaoPrimitivos {
     public static void main(String[] args) {
-
+        /*
+        * Dados não primitivos: String, Array, Class, enum
+        * Objetivo: Criar um ninja e atribuir métodos a ele
+         */
+        String nome = "Naruto Uzumaki";
+        String nomeToUpperCase = nome.toUpperCase();
+        System.out.println("nome = " + nomeToUpperCase);
+        
+        String aldeia = "ALDEIA DA FOLHA";
+        String aldeiaToLowerCase = aldeia.toLowerCase();
+        System.out.println("aldeiaToLowerCase = " + aldeiaToLowerCase);
     }
 }
